5c515b81dd249c4f3fd88fddab67b3168d3f0d75
ghoulboii <mishradharun@protonmail.com>
2022-03-10 02:59:22 +0530

    Finishing Part 1

--- base_mbr.sh
+++ base_mbr.sh
@@ -1,12 +1,28 @@
 #!/bin/bash 
 
+# Part 1: Getting Partitions Ready and Stuff
 echo "GhoulBoi's ArchInstaller Script"
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 10/" /etc/pacman.conf
 pacman --noconfirm -Sy 
 timedatectl set-ntp true
 lsblk
-echo "Enter drive (Ex. - /dev/sda): "
-read drive
+read -p "Enter drive (Ex. - /dev/sda): " drive
 cfdisk $drive
-echo "Enter the Linux Partition: "
-read linux
+read -p "Enter the Linux Partition: " linux
+mkfs.btrfs -L Linux $linux 
+read -p "Was a SWAP parition created? [y/n]: " swapcreation
+if [[ $swapcreation = y ]] ; then
+  read -p "Enter SWAP partition (Ex. - /dev/sda2): " swap
+  mkswap $swap
+  swapon $swap
+fi
+
+mount $linux /mnt
+pacstrap /mnt base base-devel linux-zen linux-firmware intel-ucode 
+genfstab -U /mnt >> /mnt/etc/fstab
+sed '1,/^#Part 2$/d' `basename $0` > /mnt/arch_install2.sh
+chmod +x /mnt/arch_install2.sh
+arch-chroot /mnt ./arch_install2.sh
+
+# Part 2: Users and Base System
+
