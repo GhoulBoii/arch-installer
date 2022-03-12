94c00b19630b4355d0216ac1e34fd162470313b3
ghoulboii <mishradharun@protonmail.com>
2022-03-12 20:40:08 +0530

    SCRIPT Directory

--- base_mbr.sh
+++ base_mbr.sh
@@ -1,24 +1,32 @@
-#!/bin/bash 
+#!/usr/bin/env  bash 
 
-# Part 1: Getting Partitions Ready and Stuff
+# Part 1: Getting Partitions Ready
 
-echo "GhoulBoi's ArchInstaller Script"
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "GhoulBoi's Arch Installer"
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 10/" /etc/pacman.conf
-pacman --noconfirm -Sy 
+pacman --noconfirm -Sy archlinux-keyring 
 timedatectl set-ntp true
 lsblk
 read -p "Enter drive (Ex. - /dev/sda): " drive
 cfdisk $drive
-read -p "Enter the Linux Partition: " linux
+read -p "Enter the Linux Partition (Ex. - /dev/sda2): " linux
+read -p "Enter SWAP partition (Enter \"n\" if no SWAP): " swapcreation
+read -p "Enter EFI partition (Enter \"n\" if using BIOS): " bios
+read -p "Enter the hostname: " hostname
 mkfs.btrfs -L Linux $linux 
-read -p "Was a SWAP parition created? [y/n]: " swapcreation
-if [[ $swapcreation = y ]] ; then
-  read -p "Enter SWAP partition (Ex. - /dev/sda2): " swap
-  mkswap $swap
-  swapon $swap
-fi
-
 mount $linux /mnt
+case $swapcreation in
+  /dev/*)
+    mkswap $swapcreation
+    swapon $swap
+    ;;
+esac
+case $bios in
+  /dev/*)
+    mkfs.fat -F 32 $bios
+    ;;
+esac 
 pacstrap /mnt base base-devel linux-zen linux-firmware intel-ucode 
 genfstab -U /mnt >> /mnt/etc/fstab
 sed '1,/^#Part 2$/d' `basename $0` > /mnt/arch_install2.sh
@@ -28,13 +36,12 @@ arch-chroot /mnt ./arch_install2.sh
 # Part 2: Users and Base System
 
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 10/" /etc/pacman.conf
-sed -i '/^#\[multilib]/{N;s/\n#/\n/}' /etc/pacman.conf
+gawk -i inplace '$0=="#[multilib]"{c=2} c&&c--{sub(/#/,"")} 1' /etc/pacman.conf 
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-read -p "Enter the hostname: " hostname
 echo $hostname > /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
@@ -43,4 +50,4 @@ mkinitcpio -P
 passwd
 pacman -S --no-confirm grub os-prober networkmanager reflector linux-headers xdg-user-dirs xdg-utils pipewire pipewire-pulse openssh tlp \
   virt-manager qemu virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat flatpak ntfs-3g
-read -P 
+read -P    
