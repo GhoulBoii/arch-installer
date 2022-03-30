652afaa6399e6a040985f894a5fa6e81e206dabd
ghoulboii <mishradharun@protonmail.com>
2022-03-30 18:46:48 +0530

    Revising and Completing

--- arch-installer.sh
+++ arch-installer.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash 
-# Part 1: Getting Partitions Ready
+# Part 1: Partition Setup
 
+clear
 echo "GhoulBoi's Arch Installer"
+echo "Part 1: Partition Setup"
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 10/" /etc/pacman.conf
 pacman --noconfirm -Sy archlinux-keyring 
 timedatectl set-ntp true
@@ -31,11 +33,12 @@ esac
 pacstrap /mnt base base-devel linux-zen linux-firmware btrfs-progs intel-ucode 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Part 2: Users and Base System
+# Part 2: Base System
 
-printf '\033c'
+clear
+echo "Part 2: Base System"
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 10/" /mnt/etc/pacman.conf
-ln -sf /usr/share/zoneinfo/Asia/Kolkata /mnt/etc/localtime
+ln -sf /mnt/usr/share/zoneinfo/Asia/Kolkata /mnt/etc/localtime
 arch-chroot /mnt hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
@@ -47,7 +50,7 @@ echo "127.0.1.1 $hostname.localdomain $hostname" >> /mnt/etc/hosts
 echo "Enter your root password: "
 arch-chroot /mnt passwd
 arch-chroot /mnt pacman -Sy --noconfirm grub os-prober networkmanager reflector linux-headers xdg-user-dirs xdg-utils pipewire pipewire-pulse openssh tlp \
-  virt-manager qemu virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat flatpak ntfs-3g tlp
+  virt-manager qemu virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat flatpak ntfs-3g tlp zsh zsh-fast-syntax-highlighting
 case $bios in
      /dev/*)
         arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
@@ -57,9 +60,24 @@ case $bios in
         arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
     ;;
 esac
-arch-chroot /mnt systemctl enable NetworkManager
-arch-chroot /mnt systemctl enable tlp
-arch-chroot /mnt systemctl enable reflector.timer
-archroot /mnt useradd -mG libvirt wheel $username
+arch-chroot /mnt systemctl enable NetworkManager tlp reflector.timer
+archroot /mnt useradd -mG libvirt wheel -s /bin/zsh $username
 arch-chroot /mnt passwd $username
 sed -i '/# %wheel ALL=(ALL:ALL) ALL/s/^#//' /mnt/etc/sudoers
+
+# Part 3: Graphical Interface
+
+clear
+cd $HOME
+git clone --depth=1 https://github.com/ghoulboii/dwm.git ~/.local/src/dwm
+sudo make -C ~/.local/src/dwm install
+git clone --depth=1 https://github.com/ghoulboii/dmenu.git ~/.local/src/dmenu
+sudo make -C ~/.local/src/dmenu install
+git clone --depth=1 https://github.com/Jguer/yay ~/.local/src/yay
+cd ~/.local/src/yay
+makepkg -si
+rm -rf ~/.local/src/yay
+cd
+ln -sf ~/.config/shell/profile ~/.zprofile
+config config --local status.showUntrackedFiles no
+exit
