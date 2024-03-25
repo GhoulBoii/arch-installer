ea8200b5577d9e91ad3b485f6148c2dbc4e70011
GhoulBoi69 <github@ghoulcloud.slmail.me>
2024-03-25 20:59:56 +0530

    Update arch-installer.sh

--- arch-installer.sh
+++ arch-installer.sh
@@ -127,8 +127,7 @@ arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 echo -e "\e[1;32mUSER CREATION\e[0m"
 arch-chroot /mnt systemctl enable NetworkManager libvirtd paccache.timer bluetooth
-arch-chroot /mnt useradd -mG wheel -s /bin/zsh $username
-arch-chroot /mnt usermod -aG libvirt $username
+arch-chroot /mnt useradd -mG wheel,libvirt -s /bin/zsh $username
 arch-chroot /mnt <<EOF
 echo "$username:$pass1" | chpasswd
 EOF
